cb881e8fd743af95a4939d65fd551b5d1425b91e
MattDurrin <25963998+MattDurrin@users.noreply.github.com>
2018-08-15 20:31:16 -0600

    Additional access removal options added

--- O365-Lockdown.ps1
+++ O365-Lockdown.ps1
@@ -1,9 +1,10 @@
-﻿<# Create the Exchange PS Session #>
+<# Create the Exchange PS Session #>
 $UserCredential = Get-Credential
 $Session = New-PSSession –ConfigurationName Microsoft.Exchange –ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential –Authentication Basic -AllowRedirection
 Import-PSSession $Session
 
 <# Enable logging for all accounts, enable all owner log actions, and retain for 365 days #>
+Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
 Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -AuditEnabled $true -AuditLogAgeLimit 365
 Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -AuditOwner Create,HardDelete,MailboxLogin,Move,MoveToDeletedItems,SoftDelete,Update
 Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-Mailbox -AuditDelegate Create,FolderBind,SendAs,SendOnBehalf,SoftDelete,HardDelete,Update,Move,MoveToDeletedItems,UpdateFolderPermissions
@@ -12,9 +13,44 @@ Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox
 Write-Host "Audit logging activation results"
 Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Select Name,Audit*
 <#
-    The following lines will disable POP3 and IMAP access. The same commands can also be used to disable MAPI, EWS, ActiveSync, and OWA access if desired by 
-    copying the content on line 18 and replacing "-PopEnabled" with "-MAPIEnabled", "-EWSEnabled", "-ActiveSyncEnalbed", or "-OWAEnabled"
+    The following lines will disable various mailbox access methods. Each line will prompt for input prior to shutting down access.
 #>
+
+<# Disable Outlook Web Access. This can be enabled on a per user basis if there is a need for it #>
+Write-Host "Would you like to disable Outlook Web Access? y/n"
+$ReadHost = Read-Host " ( y / n ) "
+Switch($ReadHost)
+{
+    y {Write-Host "Outlook Web Access Disabled"; Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-CASMailbox -OWAEnabled $False}
+    Default {Write-Host "Outlook Web Access still active"}
+}
+
+<# Disable ActiveSync. This can be enabled on a per user basis if there is a need for it #>
+Write-Host "Would you like to disable ActiveSync? y/n"
+$ReadHost = Read-Host " ( y / n ) "
+Switch($ReadHost)
+{
+    y {Write-Host "ActiveSync Disabled"; Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-CASMailbox -ActiveSyncEnalbed $False}
+    Default {Write-Host "ActiveSync still active"}
+}
+
+<# Disable MAPI Access. This can be enabled on a per user basis if there is a need for it #>
+Write-Host "Would you like to disable MAPI access? y/n"
+$ReadHost = Read-Host " ( y / n ) "
+Switch($ReadHost)
+{
+    y {Write-Host "MAPI Disabled"; Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-CASMailbox -MAPIEnabled $False}
+    Default {Write-Host "MAPI still active"}
+}
+
+<# Disable EWS Access. This can be enabled on a per user basis if there is a need for it #>
+Write-Host "Would you like to disable EWS access? y/n"
+$ReadHost = Read-Host " ( y / n ) "
+Switch($ReadHost)
+{
+    y {Write-Host "EWS Disabled"; Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Set-CASMailbox -EWSEnabled $False}
+    Default {Write-Host "EWS still active"}
+}
 
 <# Disable POP3 Access. This can be enabled on a per user basis if there is a need for it #>
 Write-Host "Would you like to disable POP3 access? y/n"
@@ -35,7 +71,7 @@ Switch($ReadHost)
 }
 
 <#
-    The following lines will restrict the ability to auto-forward to an external domain. 
+    The following lines will restrict the ability to auto-forward to an external domain. Manual forwarding is still possible.
 #>
 
 <# Remove any existing forwarding rules at the users discretion #>
